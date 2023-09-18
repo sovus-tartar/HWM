7d2a4a27e1807ab5f4744bec3895d88a0634f215
sovus_tartar <vad-nesmejanov@yandex.ru>
2023-09-18 13:44:20 +0300

    tests until 100x100 passed

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <fstream>
 
 #include "../include/matrix.hpp"
 
@@ -20,8 +21,6 @@ TEST(matrix_base, init_test)
     EXPECT_DOUBLE_EQ(A[1][0], 4.0);
     EXPECT_DOUBLE_EQ(A[1][1], 5.0);
     EXPECT_DOUBLE_EQ(A[1][2], 6.0);
-
-
 }
 
 TEST(matrix_base, switch_collumnes)
@@ -46,7 +45,6 @@ TEST(matrix_base, switch_collumnes)
     EXPECT_DOUBLE_EQ(A[1][0], 6.0);
     EXPECT_DOUBLE_EQ(A[1][1], 5.0);
     EXPECT_DOUBLE_EQ(A[1][2], 4.0);
-    
 }
 
 TEST(matrix_base, switch_lines)
@@ -70,9 +68,6 @@ TEST(matrix_base, switch_lines)
     EXPECT_DOUBLE_EQ(A[1][0], 1.0);
     EXPECT_DOUBLE_EQ(A[1][1], 2.0);
     EXPECT_DOUBLE_EQ(A[1][2], 3.0);
-
-
-
 }
 
 TEST(SquareMatrix, get_pivot1)
@@ -92,7 +87,6 @@ TEST(SquareMatrix, get_pivot1)
 
     EXPECT_EQ(t.x, 2);
     EXPECT_EQ(t.y, 2);
-    
 }
 
 TEST(SquareMatrix, get_pivot2)
@@ -112,7 +106,6 @@ TEST(SquareMatrix, get_pivot2)
 
     EXPECT_EQ(t.x, 0);
     EXPECT_EQ(t.y, 2);
-    
 }
 
 TEST(SquareMatrix, eliminate)
@@ -130,7 +123,7 @@ TEST(SquareMatrix, eliminate)
 
     MyMatrix::SquareMatrixTest::test_eliminate(A, 0);
 
-    EXPECT_DOUBLE_EQ(A[0][0], 1);    
+    EXPECT_DOUBLE_EQ(A[0][0], 1);
     EXPECT_DOUBLE_EQ(A[0][1], 2);
     EXPECT_DOUBLE_EQ(A[0][2], 3);
     EXPECT_DOUBLE_EQ(A[1][0], 0);
@@ -214,7 +207,6 @@ TEST(matrix_base, operator_equal3)
     B[1][0] = 2;
     B[1][1] = 4;
 
-
     EXPECT_FALSE(A == B);
 }
 
@@ -276,7 +268,7 @@ TEST(matrix_base, copy_assign2)
 
 TEST(square_matrix, det1)
 {
-    MyMatrix::SquareMatrix A(2);   
+    MyMatrix::SquareMatrix A(2);
     A[0][0] = 1;
     A[0][1] = 2;
     A[1][0] = 3;
@@ -285,8 +277,123 @@ TEST(square_matrix, det1)
     EXPECT_EQ(MyMatrix::SquareMatrix<double>::det(A), -10.0);
 }
 
+TEST(square_matrix, det2)
+{
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    MyMatrix::SquareMatrix A(3);
+    A[0][0] = 1;
+    A[0][1] = 2;
+    A[0][2] = 3;
+    A[1][0] = 0;
+    A[1][1] = 4; // 1 * 4 * 9
+    A[1][2] = 6;
+    A[2][0] = 0;
+    A[2][1] = 0;
+    A[2][2] = 9;
+
+    EXPECT_EQ(MyMatrix::SquareMatrix<double>::det(A), 36.0);
+}
+
+TEST(SquareMatrix, det3)
+{
+    std::ifstream file;
+
+    file.open("../test/111.in");
+
+    int N;
+    file >> N;
+
+    MyMatrix::SquareMatrix A(N);
+
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            file >> A[i][j];
+
+    double expected_det;
+
+    file >> expected_det;
+
+    file.close();
+
+    EXPECT_NEAR(MyMatrix::SquareMatrix<double>::det(A), expected_det, 1);
+}
+
+TEST(SquareMatrix, det4)
+{
+    std::ifstream file;
+
+    file.open("../test/222.in");
+
+    int N;
+    file >> N;
+
+    MyMatrix::SquareMatrix A(N);
+
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            file >> A[i][j];
+
+    double expected_det;
+
+    file >> expected_det;
+
+    file.close();
+
+    
+
+    EXPECT_NEAR(MyMatrix::SquareMatrix<double>::det(A), expected_det, 0.000000001);
+}
+
+TEST(SquareMatrix, det5)
+{
+    std::ifstream file;
+
+    file.open("../test/333.in");
+
+    int N;
+    file >> N;
+
+    MyMatrix::SquareMatrix A(N);
+
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            file >> A[i][j];
+
+    double expected_det;
+
+    file >> expected_det;
+
+    file.close();
+
+    EXPECT_NEAR(MyMatrix::SquareMatrix<double>::det(A), expected_det, 0.00000001);
+}
+
+TEST(SquareMatrix, det6)
+{
+    std::ifstream file;
+
+    file.open("../test/444.in");
+
+    int N;
+    file >> N;
+
+    MyMatrix::SquareMatrix A(N);
+
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            file >> A[i][j];
+
+    double expected_det;
+
+    file >> expected_det;
+
+    file.close();
+
+    EXPECT_NEAR(MyMatrix::SquareMatrix<double>::det(A), expected_det, 0.00000001);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
